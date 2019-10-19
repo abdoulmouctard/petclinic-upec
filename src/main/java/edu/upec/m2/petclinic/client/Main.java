34538376b233992af76e73f76cc6d5593e89ee7c
Salifou BERTHE <salifou.berthe@gmail.com>
2019-10-20 01:07:49 +0200

    INITIAL COMMIT

---
+++ src/main/java/edu/upec/m2/petclinic/client/Main.java
@@ -0,0 +1,18 @@
+package edu.upec.m2.petclinic.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**Main class to launch petclinic application in console mode.
+ * 
+ * @author Salifou BERTHE
+ *
+ */
+public class Main {
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	public static void main(String[] args) {
+		LOG.debug("======================START=========================");
+		
+		LOG.debug("======================END============================");
+	}
+}
